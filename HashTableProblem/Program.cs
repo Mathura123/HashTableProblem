0477654d87fcf26b0570b5dce5d451bcf56a6615
Mathura <mathuradas333@gmail.com>
2020-10-26 09:47:29 +0530

    [Mathura]Add. Ability to find frequency of sentance using HashTable

---
+++ HashTableProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HashTableProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Problem in Hash Table Assignment");
+            Console.WriteLine("===========================================");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(5);
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            hash.GetFrequency("or");
+        }
+    }
+}
